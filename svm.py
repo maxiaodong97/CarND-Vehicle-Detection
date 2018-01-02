a760099ca075debfa11430add88eacdb0483892b
adam.ma <adam.ma@f5.com>
2018-01-01 23:56:05 -0800

    update svm model

--- svm.py
+++ svm.py
@@ -13,12 +13,12 @@ cars = glob.glob('data/vehicles/*/*.png')
 notcars = glob.glob('data/non-vehicles/*/*.png')
 
 color_space = 'YCrCb'  # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
-orient = 13  # HOG orientations
-pix_per_cell = 8  # HOG pixels per cell
+orient = 16  # HOG orientations
+pix_per_cell = 16  # HOG pixels per cell
 cell_per_block = 2  # HOG cells per block
 hog_channel = 'ALL'  # Can be 0, 1, 2, or "ALL"
 spatial_size = (32, 32)  # Spatial binning dimensions
-hist_bins = 64    # Number of histogram bins
+hist_bins = 32    # Number of histogram bins
 spatial_feat = True  # Spatial features on or off
 hist_feat = True  # Histogram features on or off
 hog_feat = True  # HOG features on or off
